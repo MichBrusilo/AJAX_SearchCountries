ecc601472645be6dde166505d027c08239f77463
Michał Brusiło <mibru@interia.pl>
2018-05-07 14:45:28 +0200

    Finish primary version of scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,22 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = $('#countries');
+
+$('#search').click(searchCountries);
+
+function searchCountries() {
+ 	var countryName = $('#country-name').val();
+if(!countryName.length) countryName = 'Poland';
+var url = 'https://restcountries.eu/rest/v2/name/'; 
+$.ajax({
+  		url: url + countryName,
+  		method: 'GET',
+  		success: showCountriesList
+  	});
+}
+
+function showCountriesList(resp) {
+  countriesList.empty();
+  resp.forEach(function(item){
+   	$('<li>').text(item.name).appendTo(countriesList);
+});
+}
